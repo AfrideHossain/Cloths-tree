f50405f667735404f170dc4dfe7885a9311e590b
AfrideHossain <asafridehossain171@gmail.com>
2024-05-22 21:24:00 +0600

    Added: Banner in Home page; Modified:router

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomeLayout from "../layouts/HomeLayout";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
